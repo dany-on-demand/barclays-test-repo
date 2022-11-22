12bbf0f98d6b717d675425ceef89ad3cd39d203e
Daniel <dany@airmail.cc>
2022-11-22 18:42:12 +0100

    Implement basic features

--- Battleships.Test/GameTest.cs
+++ Battleships.Test/GameTest.cs
@@ -15,7 +15,31 @@ namespace Battleships.Test
         {
             var ships = new[] { "3:2,3:5" };
             var guesses = new[] { "7:0", "3:3" };
-            Game.Play(ships, guesses).Should().Be(0);
+            Game.Play(ships, guesses).Should().Be(1);
         }
+
+
+        // this is fuzzy test for fun, not an actual unit test the way XUnit understands it
+        [Fact]
+        public void TestRandomGuess()
+        {
+            var ships = new[] { "3:2,3:5" };
+            var random = new Random(); //
+            var random_y = random.Next(2, 5 + 1);
+            var guesses = new[] { $"3:{random_y}" };
+            Game.Play(ships, guesses).Should().Be(1);
+        }
+
+        //[Fact]
+        //public void TestBounds()
+        //{
+        //    // assert that app fails/throws if ships are out of the 10x10 grid
+        //}
+
+        //[Fact]
+        //public void TestInvalidShipDefinition()
+        //{
+        //    // assert that app fails/throws if there is a ship definition that's diagonal
+        //}
     }
 }
--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -20,7 +20,81 @@ namespace Battleships
         // returns: the number of ships sunk by the set of guesses
         public static int Play(string[] ships, string[] guesses)
         {
-            return 0;
+            var grid = new bool[10, 10];
+
+            foreach (string ship_definition in ships)
+            {
+                var ship = Ship.FromString(ship_definition);
+
+                // iterate over each cell in the ship
+                foreach (var cell in ship.Cells)
+                {
+                    // set the cell to true
+                    grid[cell.X, cell.Y] = true;
+                }
+            }
+
+            // track how many ships sunk
+            var ships_sunk = 0;
+
+            foreach (string guess in guesses)
+            {
+                var cell = Cell.FromString(guess);
+
+                if (grid[cell.X, cell.Y])
+                {
+                    ships_sunk++;
+                }
+            }
+
+            return ships_sunk;
         }
     }
+
+    public class Ship {
+        public List<Cell> Cells = new List<Cell>();
+
+        public static Ship FromString( string ship_definition )
+        {
+            var new_ship = new Ship();
+
+            var parts = ship_definition.Split(',');
+
+            var beginning = Cell.FromString(parts[0]);
+            var end = Cell.FromString(parts[1]);
+
+            for (int x = beginning.X; x <= end.X; x++)
+            {
+                for (int y = beginning.Y; y <= end.Y; y++)
+                {
+                    new_ship.Cells.Add(new Cell(x, y));
+                }
+            }
+
+            return new_ship;
+        }
+    }
+
+    public class Cell
+    {
+        public int X;
+        public int Y;
+
+        public Cell(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static Cell FromString(string cell_definition)
+        {
+            var parts = cell_definition.Split(':');
+
+            var x = int.Parse(parts[0]);
+            var y = int.Parse(parts[1]);
+
+            return new Cell(x, y);
+        }
+    }
+
 }
